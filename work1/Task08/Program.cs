16121aff077169959d176386c0a4cafa82505dec
Yury <yrioksnet@yandex.ru>
2022-12-20 14:25:34 +0300

    Task08

---
+++ work1/Task08/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе
+// показывает все четные числа от 1 до N.
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int i = 2;
+if(N > 1)
+{
+  while(i <= N)
+  {
+    Console.Write(i+ " " );
+    i = i + 2;
+  }
+}
+
+
